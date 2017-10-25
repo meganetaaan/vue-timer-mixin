16be6365119dd7636a9c37b7649c92e041b7c01e
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2017-10-25 22:52:26 +0900

    Refactor

--- index.js
+++ index.js
@@ -1,101 +1,15 @@
-class TimerMixin {
-  constructor(param) {
-    const evNamesDict = {
-      start: param.start,
-      pause: param.pause,
-      resume: param.resume,
-      stop: param.stop,
-      reset: param.reset
-    };
-    const tick = param.tick;
-    this.data = function() {
-      return {
-        _mode: 'countup',
-        _evNamesDict: evNamesDict,
-        time: 0,
-        _lastTime: 0,
-        _lastStarted: null,
-        _lastPassed: null,
-        _tick: tick,
-        _handler: null
-      };
-    };
-    this.created = this._created;
-    this.methods = {
-      start: this._start,
-      pause: this._pause,
-      resume: this._resume,
-      stop: this._stop,
-      reset: this._reset
-    };
-  }
-  _created() {
-    ["reset", "stop", "reset"].forEach(evKey => {
-      if (this.$data._evNamesDict[evKey] != null) {
-        let evNames = this.$data._evNamesDict[evKey];
-        if (typeof evNames === "string") {
-          evNames = [evNames];
-        }
-        if (Array.isArray(evNames)) {
-          evNames.forEach(evName => {
-            this.$on(evName, function() {
-              this[evKey].apply(this);
-              this.$emit("timer" + evName, {
-                time: this.time
-              });
-            });
-          });
-        }
-      }
-    });
-  }
-  _start() {
-    if (this.$data._handler != null) {
-      return
-    }
-    this.$data._lastStarted = Date.now()
-    this.$data._lastPassed = 0
-    this.$data._handler = setInterval(() => {
-      this.$data._lastPassed = Date.now() - this.$data._lastStarted
-      this.time = this.$data._lastTime + this.$data._lastPassed
-      this.$emit('tick', {
-        time: this.time
-      })
-    }, this.$data._tick)
-  }
-  _stop() {
-    if (this.$data._handler == null) {
-      return
-    }
-    this.$data._lastTime = this.$data._lastTime + Date.now() - this.$data._lastStarted
-    this.$data._lastStarted = null
-    this.$data._lastPassed = null
-    clearInterval(this.$data._handler)
-    this.$data._handler = null
-  }
-  _reset() {
-    if (this.$data._handler != null) {
-      this.$data._lastStarted = null
-      this.$data._lastPassed = null
-      clearInterval(this.$data._handler)
-      this.$data._handler = null
-    }
-    this.time = 0
-    this.$data._lastTime = 0
-  }
-}
-
+/* global Vue, TimerMixin */
 var myMixin = new TimerMixin({
-  start: "start",
-  stop: "stop",
-  reset: "reset",
+  start: 'start',
+  stop: 'stop',
+  reset: 'reset',
   tick: 33
-});
+})
 
 var app = new Vue({
   mixins: [myMixin],
-  name: "World",
-  el: "#app",
+  name: 'World',
+  el: '#app',
   template: `
       <div class="app">
         <div class="greeting">{{greeting}}</div>
@@ -105,21 +19,23 @@ var app = new Vue({
         <input type="button" value="reset" @click="onClickReset">
       </div>
       `,
-  data: function() {
+  data: function () {
     return {
-      greeting: "hello world!",
+      greeting: 'hello world!',
       time: null
-    };
+    }
   },
   methods: {
-    onClickStart: function() {
-      this.$emit("start");
+    onClickStart: function () {
+      this.$emit('start')
     },
-    onClickStop: function() {
-      this.$emit("stop");
+    onClickStop: function () {
+      this.$emit('stop')
     },
-    onClickReset: function() {
-      this.$emit("reset");
+    onClickReset: function () {
+      this.$emit('reset')
     }
   }
-});
+})
+
+console.debug(app)
